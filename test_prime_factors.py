4c0a4d65a6139fafa6b2b7e0130238f13b69f5ca
Ho <ho@CRA.kr>
2024-06-13 09:47:25 +0900

    [feature] page 31

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -7,9 +7,12 @@ class PrimeFactorTest(TestCase):
     def setUp(self) -> None:
         super().setUp()
         self.prime_factor = PrimeFactor()
-    def test_prime_factor_of_1(self):
-        self.assertEqual([],self.prime_factor.of(1))
 
+    def test_prime_factor_of_1(self):
+        self.assertEqual([], self.prime_factor.of(1))
 
     def test_prime_factor_of_2(self):
-        self.assertEqual([2],self.prime_factor.of(2))
+        self.assertEqual([2], self.prime_factor.of(2))
+
+    def test_prime_factor_of_3(self):
+        self.assertEqual([3], self.prime_factor.of(3))
